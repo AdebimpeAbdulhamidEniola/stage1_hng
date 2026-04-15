7ba399b13b5f6222bfde8f3ced9ad04ad27afe8b
AdebimpeAbdulhamidEniola <adebimpeabdulhamid5@gmail.com>
2026-04-15 21:19:13 +0100

    update package.json and tsconfig.json

--- dist/src/routes/profile.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createUserProfile, getProfileById, getAllProfiles, deleteProfile, } from "../controller/profile.controller";
-const router = Router();
-router.post("/", createUserProfile);
-router.get("/", getAllProfiles);
-router.get("/:id", getProfileById);
-router.delete("/:id", deleteProfile);
-export default router;
--- dist/src/utils/notfound.utils.js
+++
@@ -1,4 +0,0 @@
-import { sendError } from "./response.utils";
-export const notFoundHandler = (req, res) => {
-    sendError(res, 404, `Route ${req.originalUrl} not found`);
-};
